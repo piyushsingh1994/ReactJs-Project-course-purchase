14a59e197603b387e195bfbd95f7d8d9498e3a7b
Piyush Singh <piyushkumar194@gmail.com>
2018-11-24 16:06:56 +0530

    Updates

---
+++ precache-manifest.135dc99e09b0408f44ee07d5412f5ab6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d31c2400b4ec28b73c2d",
+    "url": "/ReactJs-Project-course-purchase/static/css/main.c2bd6bae.chunk.css"
+  },
+  {
+    "revision": "d31c2400b4ec28b73c2d",
+    "url": "/ReactJs-Project-course-purchase/static/js/main.d31c2400.chunk.js"
+  },
+  {
+    "revision": "de0d630cc687d3f4976d",
+    "url": "/ReactJs-Project-course-purchase/static/js/1.de0d630c.chunk.js"
+  },
+  {
+    "revision": "3d88a680ce680ad584a7",
+    "url": "/ReactJs-Project-course-purchase/static/js/runtime~main.3d88a680.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactJs-Project-course-purchase/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4d55ee1884fc133ac74f729780e748e0",
+    "url": "/ReactJs-Project-course-purchase/index.html"
+  }
+];
